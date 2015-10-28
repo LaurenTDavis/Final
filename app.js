1c41e0a3fdeea4ee9ca276c090d52fbc72ee3d54
Lauren Davis <londond9218@gmail.com>
2015-10-27 19:40:57 -0600

    first commit

---
+++ app.js
@@ -0,0 +1,70 @@
+// Requires \\
+var express = require('express');
+var bodyParser = require('body-parser');
+
+// Connect to DB
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/final')
+
+// Auth Requires
+var session = require('express-session');
+var passport = require('passport');
+
+var passportConfig = require('./config/passport'); 
+
+// Create Express App Object \\
+var app = express();
+
+// Session Setup
+app.use(session({
+	secret: 'secret',
+	resave: true,
+	saveUninitialized: false
+}));
+
+// Hook in passport to the middleware chain
+app.use(passport.initialize());
+
+// Hook in the passport session management into the middleware chain.
+app.use(passport.session());
+
+app.use(express.static(__dirname + '/public'));
+
+// Application Configuration \\
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Routes \\
+var authenticationController = require('./controllers/authentication');
+
+// Our get request for viewing the login page
+app.get('/auth/login', authenticationController.login);
+
+// Post received from submitting the login form
+app.post('/auth/login', authenticationController.processLogin);
+
+// Post received from submitting the signup form
+app.post('/auth/signup', authenticationController.processSignup);
+
+// Any requests to log out can be handled at this url
+app.get('/auth/logout', authenticationController.logout);
+
+// This route is designed to send back the logged in user (or undefined if they are NOT logged in)
+app.get('/api/me', function(req, res){
+	res.send(req.user)
+})
+
+// app.use(passportConfig.ensureAuthenticated);
+
+app.get('/', function(req, res){
+  res.sendFile('/html/home.html', {root : './public'})
+});
+
+
+
+// Creating Server and Listening for Connections \\
+var port = 3000
+app.listen(port, function(){
+  console.log('Server running on port ' + port);
+
+});
---
+++ public/js/main.js
@@ -0,0 +1,61 @@
+angular.module('App', ['ngRoute']);
+
+
+angular.module('App')
+	.config(['$routeProvider', function($routeProvider){
+		// No need to define #, it is assumed
+		$routeProvider
+			.when('/', {
+				templateUrl : '/html/index.html',
+				controller : 'mainController'
+			})
+			.when('/auth/login', {
+				templateUrl : '/html/login.html',
+				controller : 'authService'
+			})
+			.when('/locate', {
+				templateUrl : '/html/locate.html',
+				controller: 'locateController'
+			})
+
+	}])
+
+
+angular.module('App')
+	.service('authService', ['$http', '$location', '$routeParams', function($http, $routeParams){
+		var username = $routeParams.username
+		console.log(username)
+		
+		this.authCheck = function(cb){
+			$http.get('/api/me')
+				.then( function(returnData){
+					cb(returnData.data)
+
+				})
+		}
+					
+						
+	}])
+
+angular.module('App')
+	.controller('mainController', ['$scope', '$http', 'authService', '$routeParams', function($scope, $http, authService, $routeParams){
+		console.log('AUTH', authService)
+	
+	$scope.hello = "Food";
+		
+		authService.authCheck(function(user){
+			console.log('USER!', user)
+			$scope.user = user
+		})
+
+	}])
+
+angular.module('App')
+	.controller('locateController', ['$scope', '$http', '$routeParams', function($scope, $http, $routeParams){
+		
+	$scope.hello = "Food";
+	
+		
+
+	}])
+	
