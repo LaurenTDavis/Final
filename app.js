7e1b6db7fe08349e1adf9c985dbcc0a4153fe749
Lauren Davis <londond9218@gmail.com>
2015-11-01 10:13:55 -0700

    got rid of schedules

--- app.js
+++ app.js
@@ -70,42 +70,42 @@ app.get('/', function(req, res){
 
 
 // Creating Server and Listening for Connections \\
-// var port = 3000
-// app.listen(port, function(){
-//   console.log('Server running on port ' + port);
+var port = 3000
+app.listen(port, function(){
+  console.log('Server running on port ' + port);
 
+});
+
+
+// // catch 404 and forward to error handler
+// app.use(function(req, res, next) {
+//   var err = new Error('Not Found');
+//   err.status = 404;
+//   next(err);
 // });
 
+// // error handlers
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
+// // development error handler
+// // will print stacktrace
+// if (app.get('env') === 'development') {
+//   app.use(function(err, req, res, next) {
+//     res.status(err.status || 500);
+//     res.render('error', {
+//       message: err.message,
+//       error: err
+//     });
+//   });
+// }
 
-// error handlers
+// // production error handler
+// // no stacktraces leaked to user
+// app.use(function(err, req, res, next) {
+//   res.status(err.status || 500);
+//   res.render('error', {
+//     message: err.message,
+//     error: {}
+//   });
+// });
 
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
-  });
-}
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
-});
-
-module.exports = app;
+// module.exports = app;
